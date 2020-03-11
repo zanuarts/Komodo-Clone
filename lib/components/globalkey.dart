dfa6776a963942854004cded0537412ee79426fe
zanuarts <nuartea27@gmail.com>
2020-03-11 23:18:26 +0700

    add apiwebsite backup

--- lib/components/globalkey.dart
+++ lib/components/globalkey.dart
@@ -2,5 +2,6 @@ import 'package:flutter/material.dart';
 
 // var apiwebsite = 'https://cbnusantara.id/api_komodo/';
 var apiwebsite = 'https://cbnusantara.id/api_komodo_dev/';
+// var apiwebsite = 'https://ojanhtp.000webhostapp.com/';
 Color maincolor = Color.fromRGBO(249,89,0,1);
 var deeporange = "#FF5301";
